4e3bc57ef6b6905436d283478810e9df2e51a897
Super User <root@localhost.localdomain>
2026-03-06 21:51:55 +0530

    Add Kubernetes manifests for GitOpsWave

---
+++ app/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return f"""
+    <h1>GitOpsWave Application</h1>
+    <p>GitOps Deployment using Kubernetes</p>
+    <p>Pod Hostname: {socket.gethostname()}</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
